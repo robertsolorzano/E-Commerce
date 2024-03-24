6d5abc33ba83820c40c31dfdc1c7d3a1c3a0918d
Robert-ArchLinux <robertsolorzano510@gmail.com>
2024-03-23 17:20:31 -0700

    Defined GET route based on category ID
    -Route uses 'find by primary key' or findByPk() to retrieve a category
    by its id value which is passed as a param in the URL
    -include option used to load associated products for the category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -14,9 +14,19 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    const category = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (!category) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
