85869c9597b0ee51ecf13877b48a19c1d47d1d89
Robert-ArchLinux <robertsolorzano510@gmail.com>
2024-03-25 13:31:58 -0700

    Defined GET all products route
    -This route retrieves all products along wiht their associated Category
    and tag data

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,12 +1,16 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
-// The `/api/products` endpoint
-
-// get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+// GET all products
+router.get('/', async (req, res) => {
+  try {
+    const products = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }],
+    });
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
