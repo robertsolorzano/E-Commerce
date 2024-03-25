e0e9c8b590b78a5f7df43a7a95427b6c9c1f8f81
Robert-ArchLinux <robertsolorzano510@gmail.com>
2024-03-25 14:36:23 -0700

    Defined GET route for product by id
    -Uses find by primary key to retrieve a single product by its ID along
    with its associated category and tag data

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -13,11 +13,22 @@ router.get('/', async (req, res) => {
   }
 });
 
-// get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+// GET a single product by ID
+router.get('/:id', async (req, res) => {
+  try {
+    const product = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag }],
+    });
+    if (!product) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
 
 // create new product
 router.post('/', (req, res) => {
