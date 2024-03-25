65798752c58a84bfdb7341a56c40a710c5771065
Robert-ArchLinux <robertsolorzano510@gmail.com>
2024-03-25 12:30:14 -0700

    Created DELETE route by id value
    -This route uses .destroy() to delete a category by its id value
    -If category is deleted a 200 status code is sent with a message, if no
    category is found with the provided id, a 404 status code is sent

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -53,8 +53,19 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedRows = await Category.destroy({
+      where: { id: req.params.id },
+    });
+    if (deletedRows === 0) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+    res.status(200).json({ message: 'Category deleted successfully!' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
